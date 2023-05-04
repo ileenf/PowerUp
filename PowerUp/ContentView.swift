da69198411b70bbf4c5b6e0314e10234c1f03773
ileenf <52259696+ileenf@users.noreply.github.com>
2023-05-03 20:51:07 -0700

    Add files via upload

---
+++ PowerUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PowerUp
+//
+//  Created by Ileen F on 5/3/23.
+// hello 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PowerUp/PowerUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerUpApp.swift
+//  PowerUp
+//
+//  Created by Ileen F on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PowerUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
