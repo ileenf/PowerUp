8b8f9f4da1263f8310237fe8f3c4358409604e2b
Jayson Nguyen <jaysonn@uci.edu>
2023-06-04 17:18:32 -0700

    fixed json string parsing

--- PowerUp/Views/ResultsView.swift
+++ PowerUp/Views/ResultsView.swift
@@ -7,77 +7,6 @@
 
 import SwiftUI
 
-//struct Food: Codable {
-//    let Category: String
-//    let Data: [String: Any]
-//    let Description: String
-//    let NutrientDataBankNumber: Int
-//
-//    enum CodingKeys: String, CodingKey {
-//        case Category
-//        case Data
-//        case Description
-//        case NutrientDataBankNumber
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        Category = try container.decode(String.self, forKey: .Category)
-//        Description = try container.decode(String.self, forKey: .Description)
-//        NutrientDataBankNumber = try container.decode(Int.self, forKey: .NutrientDataBankNumber)
-//        Data = try container.decode([String: AnyCodable].self, forKey: .Data).compactMapValues { $0.value }
-//    }
-//
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(Category, forKey: .Category)
-//        try container.encode(Description, forKey: .Description)
-//        try container.encode(NutrientDataBankNumber, forKey: .NutrientDataBankNumber)
-//        try container.encode(Data.mapValues(AnyCodable.init), forKey: .Data)
-//    }
-//}
-//
-//struct AnyCodable: Codable {
-//    let value: Any
-//
-//    init(_ value: Any) {
-//        self.value = value
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.singleValueContainer()
-//        if let int = try? container.decode(Int.self) {
-//            value = int
-//        } else if let double = try? container.decode(Double.self) {
-//            value = double
-//        } else if let string = try? container.decode(String.self) {
-//            value = string
-//        } else if let bool = try? container.decode(Bool.self) {
-//            value = bool
-//        } else {
-//            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unsupported type")
-//        }
-//    }
-//
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.singleValueContainer()
-//        switch value {
-//        case let int as Int:
-//            try container.encode(int)
-//        case let double as Double:
-//            try container.encode(double)
-//        case let string as String:
-//            try container.encode(string)
-//        case let bool as Bool:
-//            try container.encode(bool)
-//        default:
-//            throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: encoder.codingPath, debugDescription: "Unsupported type"))
-//        }
-//    }
-//}
-
-
-
 struct ResultsView: View {
     let firstName: String = UserDefaults.standard.string(forKey: "firstName")!
     
@@ -123,21 +52,26 @@ struct ResultsView: View {
                 if let responseString = String(data: data, encoding: .utf8) {
                     print("Response: \(responseString)")
                 }
-//                let decoder = JSONDecoder()
-//                do {
-//                    let foods = try decoder.decode([Food].self, from: data)
-//
-//                    // Access and work with the parsed data
-//                    for food in foods {
-//                        print("Category: \(food.Category)")
-//                        print("Description: \(food.Description)")
-//                        print("Nutrient Data Bank Number: \(food.NutrientDataBankNumber)")
-//                        print("Data: \(food.Data)")
-//                        print("--------")
-//                    }
-//                } catch {
-//                    print("Error parsing JSON: \(error)")
-//                }
+                do {
+                    if let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+                        for json in jsonArray {
+                            if let category = json["Category"] as? String,
+//                               let data = json["Data"] as? [String: Any],
+                               let description = json["Description"] as? String,
+                               let nutrientDataBankNumber = json["Nutrient Data Bank Number"] as? Int {
+                                
+                                // Use the extracted values here
+                                print("Category: \(category)")
+//                                print("Data: \(data)")
+                                print("Description: \(description)")
+                                print("Nutrient Data Bank Number: \(nutrientDataBankNumber)")
+                                print()
+                            }
+                        }
+                    }
+                } catch {
+                    print("Error parsing JSON: \(error)")
+                }
             }
         }
         task.resume()
